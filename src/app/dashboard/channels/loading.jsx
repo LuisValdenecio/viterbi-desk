ce75ac7da4a1d4d67d910dc98c82c91bc9fc3abc
Luis Tchitue <ls04af@gmail.com>
2024-08-27 12:51:06 +0100

    deletes all the unnecessary files for the new restructuring

--- src/app/dashboard/channels/loading.jsx
+++
@@ -1,6 +0,0 @@
-import { Skeleton_ } from './(components)/skeletons'
-
-export default function Loading({main_title, description}) {
-    // You can add any UI inside Loading, including a Skeleton.
-    return <Skeleton_ main_title={main_title} description={description} />
-}
