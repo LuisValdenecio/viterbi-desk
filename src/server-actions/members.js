574fc19cebcdc6edb57b0b9ea7690d4e906775aa
Luis Tchitue <ls04af@gmail.com>
2024-09-19 22:06:44 +0100

    adds protection against non-founder members promoting others to owners

--- src/server-actions/members.js
+++ src/server-actions/members.js
@@ -61,7 +61,7 @@ export async function reassignMemberRole(_prevstate, formData) {
             }
         }
 
-        const privilege = await checkPrivilege(team_id, member_id)
+        const privilege = await checkRolePrivilege(team_id, member_id, role)
 
         if (!privilege) {
             return {
@@ -233,6 +233,74 @@ export async function checkPrivilege(team_id, member_id) {
 
         // owner members can delete readers and admins
         if (my_role[0].role === 'owner' && member_role[0].role !== 'owner') {
+            return true
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// this function makes sure that non-authorized users 
+// promote members to certain roles
+export async function checkRolePrivilege(team_id, member_id, role) {
+    const session = await auth()
+
+    try {
+
+        // check your role on this team:
+        const my_role = await prisma.user_privilege.findMany({
+            where: {
+                user_id: session?.user?.id,
+                team_id: team_id
+            },
+            select: {
+                role: true
+            }
+        })
+
+        // if you're nononwer, you can't delete others
+        if (my_role[0].role !== 'owner') {
+            return false
+        }
+
+        // if the user attempting to delete a member is not 
+        // the founding member, he can't delete owners
+        const member_role = await prisma.user_privilege.findMany({
+            where: {
+                user_id: member_id,
+                team_id: team_id
+            },
+            select: {
+                role: true
+            }
+        })
+
+        // check if the attempting deleter is the founding member
+        const founding_member = await prisma.team.findUnique({
+            where: {
+                team_id: team_id,
+                user_id: session?.user?.id
+            }
+        })
+
+        // only the founding member can promoete to owner
+        if (!founding_member && role === 'owner') {
+            return false
+        }
+        
+        // owner members can't promote or demote other owner members
+        if (member_role[0].role === 'owner' && my_role[0].role === 'owner') {
+            return false
+        }
+
+        // promoting a member to owner
+        if (founding_member && role === 'owner') {
+            return true
+        }
+
+        // owner members can delete readers and admins
+        if (my_role[0].role === 'owner' && member_role[0].role !== 'owner' && role !== 'owner') {
             return true
         }
 
