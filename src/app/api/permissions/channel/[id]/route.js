cd31ffc026539415bf65ccd5d493fb878e4a69ff
Luis Tchitue <ls04af@gmail.com>
2024-09-23 19:42:52 +0100

    allows team members with admin privileges to create agents on the channels their teams belong to

--- src/app/api/permissions/channel/[id]/route.js
+++ src/app/api/permissions/channel/[id]/route.js
@@ -7,16 +7,51 @@ export const GET = async (req, { params }) => {
     const session = await auth()
 
     try {
+        // fetch all the teams associated with this channel
+        const teams = await prisma.team_channel.findMany({
+            where : {
+                channel_id : params.id
+            },
+            select : {
+                team_id : true
+            }
+        })
 
-        // get the id of the channel this agent belongs to
+        const teams_ids = teams.flatMap(team => team.team_id)
+
+        // find the teams I work associated with the channel
+        const my_teams_admin = await prisma.user_privilege.findMany({
+            where : {
+                user_id : session?.user?.id,
+                role : 'admin',
+                team_id : {
+                    in : teams_ids
+                }
+            }
+        })
+
+        const my_teams_owner = await prisma.user_privilege.findMany({
+            where : {
+                user_id : session?.user?.id,
+                role : 'owner',
+                team_id : {
+                    in : teams_ids
+                }
+            }
+        })
+
+        const my_teams = my_teams_admin.concat(my_teams_owner)
+
+
+        /* get the id of the channel this agent belongs to
         const privilege = await prisma.channel.findUnique({
             where : {
                 owner_id : session?.user?.id,
                 channel_id : params.id
             }   
         })
-    
-        if (!privilege) {
+        */
+        if (my_teams.length == 0) {
             return Response.json("nonowner")
         } else {
             return Response.json("owner")
